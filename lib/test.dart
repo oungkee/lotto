6f0ede48df3cb305439157bfc76b7a891a7a65f5
oungkee <oungkee@gmail.com>
2022-08-25 17:08:35 +0900

    BottomNavigationBar를 이용하여 탭으로 구분함.

--- lib/test.dart
+++ lib/test.dart
@@ -1,12 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class test extends StatelessWidget {
   const test({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('test'),
+    return ListView(
+      // !!중요!! Column 을 ListView로 변경하면 상하 스크롤이 생김.
+      children: [
+        _buildTop(),
+        _buildMiddle(),
+        _buildBottom(),
+      ],
     );
+  }
+
+  _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          // SizedBox(
+          //   height: 20,
+          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  print('클릭');
+                },
+                child: Column(
+                  children: const [
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('택시'),
+                  ],
+                ),
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('블랙'),
+                ],
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('바이크'),
+                ],
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('대리'),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: const [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('택시'),
+                ],
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('블랙'),
+                ],
+              ),
+              Column(
+                children: const [
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('바이크'),
+                ],
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Column(
+                  children: const [
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('대리'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  //-- 중간 슬라이드 구성.
+  _buildMiddle() {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 400.0, // 높이 400
+      ),
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            //context 를 사용하고자 할때
+            return Container(
+              width: MediaQuery.of(context).size.width, //기기의 가로 길이
+              margin: const EdgeInsets.symmetric(horizontal: 5.0), //좌우 여백5
+              decoration: const BoxDecoration(color: Colors.amber //배경색
+                  ),
+              child: Text(
+                'text $i',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  _buildBottom() {
+    return const Text('Bottom');
   }
 }
