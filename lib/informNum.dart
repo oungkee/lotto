0db70e342c844caafbf7597c617e3c84471b8180
oungkee <oungkee@gmail.com>
2022-12-08 16:57:11 +0900

    [@2022-12-08] 탭바 이동 시 이전 화면이 초기화 되는 문제 해결을 위해 IndexedStack 함수를 사용함.

--- lib/informNum.dart
+++ lib/informNum.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; //http 패키지 추가.
 import 'dart:convert'; // json을 수동으로 직렬화 시키기 위해 convert 패키지 추가.
+import 'dart:async';
 import 'wgseo_module.dart';
 
 class informNum extends StatefulWidget {
@@ -41,6 +42,18 @@ class _informNumState extends State<informNum> {
                 return Text(snapshot.data.toString());
               }
             }),
+        wgseo_Sized_Heigh(),
+        FutureBuilder(
+            future: fetchNowSeq(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData == false) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return Text(snapshot.data.toString());
+              }
+            }),
       ],
     ));
   }
@@ -52,12 +65,43 @@ Future fetchPost() async {
       const Duration(milliseconds: 5)); // 비동기 과정을 보여주기 위해 시간을 딜레이 시킨다.
 
   int page = 994;
-
   String lottoUrl =
       'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$page'; // 동행복권 Lotto api url
 
   final response = await http.get(Uri.parse(lottoUrl)); // http 데이터를 가져온다.
+  var LottoNums = []; // 날짜, 회차, 로또 당첨 번호
 
+  // 만약 서버 상태 코드가 200과 함께 OK 응답을 반환하면, JSON을 파싱한다.
+  if (response.statusCode == 200) {
+    final nums = await json.decode(response.body);
+
+    // 필요한 정보만을 가져온다.
+    LottoNums.add(nums['drwNoDate']); // 날짜
+    LottoNums.add(nums['drwNo']); // 회차
+    LottoNums.add(nums['drwtNo1']); // 번호1
+    LottoNums.add(nums['drwtNo2']); // 번호2
+    LottoNums.add(nums['drwtNo3']); // 번호3
+    LottoNums.add(nums['drwtNo4']); // 번호4
+    LottoNums.add(nums['drwtNo5']); // 번호5
+    LottoNums.add(nums['drwtNo6']); // 번호6
+    LottoNums.add(nums['bnusNo']); // 보너스 번호
+
+    return LottoNums;
+  } else {
+    return "Error";
+  }
+}
+
+// 비동기를 통해 네트워크 요청
+Future fetchNowSeq() async {
+  await Future.delayed(
+      const Duration(milliseconds: 5)); // 비동기 과정을 보여주기 위해 시간을 딜레이 시킨다.
+
+  int page = 994;
+  String lottoUrl =
+      'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$page'; // 동행복권 Lotto api url
+
+  final response = await http.get(Uri.parse(lottoUrl)); // http 데이터를 가져온다.
   var LottoNums = []; // 날짜, 회차, 로또 당첨 번호
 
   // 만약 서버 상태 코드가 200과 함께 OK 응답을 반환하면, JSON을 파싱한다.
