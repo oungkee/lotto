0e34bca0c715ffaf018a7e3607f15b7f109deb66
oungkee <oungkee@gmail.com>
2023-05-17 16:10:54 +0900

    [@2022-12-08] 탭바 이동 시 이전 화면이 초기화 되는 문제 해결을 위해 IndexedStack 함수를 사용함.

--- lib/informNum.dart
+++ lib/informNum.dart
@@ -14,11 +14,14 @@ class informNum extends StatefulWidget {
 }
 
 class _informNumState extends State<informNum> {
+  //해당 회차의 당첨 정보를 기록하기 위한 글러벌 Map 변수 선언.
   var data = {};
+
   @override
   void initState() {
+    // 최초 시작될 때 실행.
     super.initState();
-    // return Timer(Duration(seconds: 3), fetchNowGameNo());
+    // 1. 현 회차를 불러온다.
     fetchNowGameNo();
   }
 
@@ -33,6 +36,7 @@ class _informNumState extends State<informNum> {
           style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
         ),
         wgseo_Sized_Heigh(),
+        // data Map 에 등록된 값을 Text 값으로 표시하고, showFormatNum 함수를 통하여 천단위 , 를 표시한다.
         Text('현재 추첨 회차는 ${showFormatNum(data['drwNo'].toString())} 회 입니다'),
         wgseo_Sized_Heigh(),
         wgseo_Sized_Heigh(),
@@ -60,25 +64,32 @@ class _informNumState extends State<informNum> {
   }
 
   Future<void> fetchNowGameNo() async {
-    //현재의 추첨회차를 읽어온다.
+    //1. 현재의 추첨회차를 읽어온다.
     // await Future.delayed(
     //     const Duration(milliseconds: 2)); // 비동기 과정을 보여주기 위해 시간을 딜레이 시킨다.
 
     final response = await http
         .get(Uri.parse('https://dhlottery.co.kr/gameResult.do?method=byWin'));
 
+    // 한글이 깨지는 경우 cp949toUni 를 통하여 한글로 변환.
     var tempStr = cp949toUni(response.body.codeUnits);
 
+    // 반환된 json 데이터를 resultNo에 넣는다.
     var resultNo = tempStr.substring(
+        // substring으로 문자를 자른다
         tempStr.indexOf(
+              // indexOf 를 통하여 자를 글자의 시작과 끝점을 지정하여 Result 변수에 값을 저장한다.
               'content="동행복권 ',
             ) +
             14,
         tempStr.indexOf('회 당첨번호'));
+
+    // getJSONData 함수에 현재의 추첨 회차를 넣어 데이터를 조회한다.
     getJSONData(resultNo);
   }
 
   Future<void> getJSONData(String tempNo) async {
+    // 2. 지정된 회차의 당첨번호를 JSON으로 받아 data Map 변수에 저장한다.
     final response = await http.get(Uri.parse(
         'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$tempNo'));
     if (response.statusCode == 200) {
@@ -105,6 +116,7 @@ class _informNumState extends State<informNum> {
         //     const Duration(milliseconds: 2)); // 비동기 과정을 보여주기 위해 시간을 딜레이 시킨다.
       });
     } else {
+      // 데이터 호출에 실패하는 경우 오류 표시.
       throw Exception('API 요청실패');
     }
   }
