32331001e6fd978985bf58c59f22fb2ad9ac7cc0
oungkee <oungkee@gmail.com>
2023-05-24 10:06:04 +0900

    [@2022-12-08] 탭바 이동 시 이전 화면이 초기화 되는 문제 해결을 위해 IndexedStack 함수를 사용함.

--- lib/history.dart
+++ lib/history.dart
@@ -20,7 +20,31 @@ class _historyState extends State<history> {
     super.dispose();
   }
 
+  // 할 일 추가 메서드
+  void _addTodo(Todo todo) {
+    setState(() {
+      _items.add(todo);
+      _todoContoller.text = ''; //할 일 입력 필드를 비움.
+    });
+  }
+
+  // 할 일 삭제 메서드
+  void _deleteTodo(Todo todo) {
+    setState(() {
+      _items.remove(todo);
+    });
+  }
+
+  // 할 일 완료/미완료 메서드
+  void _toggleTodo(Todo todo) {
+    setState(() {
+      //isDone 프로퍼티값을 변경하고 setState() 함수로 UI를 다시 그리는 메서스들입니다.
+      todo.isDone = !todo.isDone;
+    });
+  }
+
   @override
+  // build 메서드
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -35,7 +59,9 @@ class _historyState extends State<history> {
                 ),
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _addTodo(Todo(_todoContoller.text));
+                },
                 child: const Text('추가'),
               ),
             ],
@@ -43,7 +69,7 @@ class _historyState extends State<history> {
           //Expanded 로 묶어야 위의 Row 열 이외의 항목은 화면을 가득 채운다.
           Expanded(
             child: ListView(
-              children: [],
+              children: _items.map((todo) => _buildItemWidget(todo)).toList(),
             ),
           )
         ],
@@ -51,21 +77,34 @@ class _historyState extends State<history> {
     );
   }
 
-//   //할일 객체를 ListTile 형태로 변경하는 메서드(함수) (Todo todo) 와 같이 클래스를 상속받아 사용할 수 있다.
-//   _buildItemWidget(Todo todo) {
-//     return ListTile(
-//       //클릭 시 완료/취소되도록 수정.
-//       onTap: () {},
-//       title: Text(
-//         // 할 일
-//         todo.title,
-//         // 완료일 때는 스타일 적용
-//         // style: todo.isDone?TextStyle(
-//         //
-//         // ),
-//       ),
-//     );
-//   }
+  //할일 객체를 ListTile 형태로 변경하는 메서드(함수) (Todo todo) 와 같이 클래스를 상속받아 사용할 수 있다.
+  Widget _buildItemWidget(Todo todo) {
+    return ListTile(
+        //클릭 시 완료/취소되도록 수정.
+        onTap: () {
+          _toggleTodo(todo); // 완료/미완료 메서드 실행.
+        },
+        title: Text(
+          // 할 일
+          todo.title,
+          // 완료일 때는 스타일 적용
+          style: todo.isDone
+              ? const TextStyle(
+                  decoration: TextDecoration.lineThrough, //완료되면 취소선
+                  fontStyle: FontStyle.italic, //이탤릭체
+                )
+              // 완료되지 않았다면 style 변경은 없다.
+              : null,
+        ),
+        trailing: IconButton(
+          // 리스트 타일의 아이콘 버튼
+          //리스트타일에 사용
+          icon: const Icon(Icons.delete_forever), //휴지통 아이콘
+          onPressed: () {
+            _deleteTodo(todo); // 삭제 메서드 실행.
+          }, //쓰레기통 클릭 시 삭제되도록.
+        ));
+  }
 }
 
 //할일 클래스
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
+import firebase_core
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
